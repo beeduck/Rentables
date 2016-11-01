da9bed15d3bb5708b067f368f0ce92cf5488f8f5
Asad <aabdullah7757@gmail.com>
2016-10-31 17:26:55 -0700

    Created Post service and controller.

---
+++ Rent/Rent-Data/src/main/java/dataAccess/entities/UserPost.java
@@ -0,0 +1,98 @@
+package dataAccess.entities;
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+
+/**
+ * Created by Asad on 10/31/2016.
+ *
+ */
+
+@Entity
+@Table(name = "UserPosts")
+public class UserPost implements Serializable {
+    private int id;
+    private String title;
+    private String description;
+    private boolean isActive;
+    private Timestamp createDate;
+    private Timestamp lastEditDate;
+    private int userId;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "title", nullable = false)
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Basic
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Basic
+    @Column(name = "isActive")
+    @Type(type = "yes_no")
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    @Basic
+    @Column(name = "createDate")
+    public Timestamp getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Timestamp createDate) {
+        this.createDate = createDate;
+    }
+
+    @Basic
+    @Column(name = "lastEditDate")
+    public Timestamp getLastEditDate() {
+        return lastEditDate;
+    }
+
+    public void setLastEditDate(Timestamp lastEditDate) {
+        this.lastEditDate = lastEditDate;
+    }
+
+    @Basic
+    @Column(name = "userId")
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
+
---
+++ Rent/Rent-Main/src/main/java/Services/UserPost/UserPostServiceImpl.java
@@ -0,0 +1,49 @@
+package Services.UserPost;
+
+import DTOEntities.UserPost.UserPostDTO;
+import dataAccess.dao.UserPost.UserPostDAO;
+import dataAccess.entities.UserPost;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Asad on 10/31/2016.
+ */
+
+@Service("userPostService")
+public class UserPostServiceImpl implements UserPostService {
+
+    @Autowired
+    private UserPostDAO userPostDAO;
+
+    public void createPost(UserPostDTO userPostDTO) {
+        UserPost post = new UserPost();
+        post.setTitle(userPostDTO.getTitle());
+        post.setDescription(userPostDTO.getDescription());
+        post.setUserId(userPostDTO.getUserId());
+        Date date = new Date();
+        Timestamp timeStamp = new Timestamp(date.getTime());
+        post.setCreateDate(timeStamp);
+        post.setLastEditDate(timeStamp);
+        userPostDAO.createPost(post);
+    }
+
+    public void updateUser(UserPost userPost) {
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date.getTime());
+        userPost.setLastEditDate(timestamp);
+        userPostDAO.updatePost(userPost);
+    }
+
+    public List<UserPost> getPostsByUserId(int userId) {
+        return userPostDAO.getPostsByUserId(userId);
+    }
+
+    public UserPost getPostById(int id) {
+        return userPostDAO.getPostById(id);
+    }
+}
