0b9bae86bfcb35a4bfe4fbcdcd6eef0d354a9581
beeduck <kristopher.morris.844@my.csun.edu>
2016-11-05 14:18:13 -0700

    User information is now returned for the user that logs in.

--- Rent/Rent-Main/src/main/java/Configuration/AuthSuccessHandler.java
+++ Rent/Rent-Main/src/main/java/Configuration/AuthSuccessHandler.java
@@ -2,18 +2,13 @@ package Configuration;
 
 import Services.User.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import dataAccess.entities.User;
+import dataAccess.entities.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JacksonJsonParser;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -27,10 +22,10 @@ import java.io.PrintWriter;
 @Component
 public class AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    private RequestCache requestCache = new HttpSessionRequestCache();
-
     @Autowired
     UserService userService;
+
+    private RequestCache requestCache = new HttpSessionRequestCache();
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -38,11 +33,9 @@ public class AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
         PrintWriter printWriter = response.getWriter();
 
-        Object object = authentication.getPrincipal();
-        String name = authentication.getName();
-        //userService.updateUserModifiedTime(user);
-
-        User user = userService.getUserByName(name);
+        String username = authentication.getName();
+        User user = userService.getUserByName(username);
+        userService.updateUserModifiedTime(user);
 
         ObjectMapper mapper = new ObjectMapper();
         String userString = mapper.writeValueAsString(user);
@@ -50,25 +43,6 @@ public class AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
         printWriter.write(userString);
 
         response.setStatus(200);
-        response.setHeader("content-type=application/json", "test");
-
-        // TODO: Return authenticated user details
-        // TODO: Update user modified timestamp
-
-//        SavedRequest savedRequest = requestCache.getRequest(request, response);
-//
-//        if (savedRequest == null) {
-//            clearAuthenticationAttributes(request);
-//            return;
-//        }
-//        String targetUrlParam = getTargetUrlParameter();
-//        if (isAlwaysUseDefaultTargetUrl() ||
-//                (targetUrlParam != null &&
-//                        StringUtils.hasText(request.getParameter(targetUrlParam)))) {
-//            requestCache.removeRequest(request, response);
-//            clearAuthenticationAttributes(request);
-//            return;
-//        }
 
         clearAuthenticationAttributes(request);
     }
