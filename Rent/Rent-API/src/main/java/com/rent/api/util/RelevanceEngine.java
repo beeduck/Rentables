671af3d59bb3ffc2cfa0312369da205fdf1b7f7d
Asad <aabdullah7757@gmail.com>
2016-11-18 09:58:02 -0800

    finished calculation of idf score, starting on query boost part

--- Rent/Rent-API/src/main/java/com/rent/api/util/RelevanceEngine.java
+++ Rent/Rent-API/src/main/java/com/rent/api/util/RelevanceEngine.java
@@ -12,18 +12,20 @@ public class RelevanceEngine {
 
     public static List<UserPost> sortByRelevance(List<UserPost> postList, UserPostFilter filter) {
         Map<UserPost,Double> hashmap = new HashMap<>();
+        Map<String,Double> idfScores = calculateIDFScore(filter.getKeywords(),postList);
         for(UserPost e : postList) {
-            String[] titleSplit = e.getTitle().split("[\\s@&.?$+-,]+");
-            String[] descriptionSplit = e.getDescription().split("[\\s@&.?$+-,]+");
+            List<String> titleSplit = Arrays.asList(e.getTitle().toLowerCase().split("[\\s@&.?$+-,]+"));
+            List<String> descriptionSplit = Arrays.asList(e.getDescription().toLowerCase().split("[\\s@&.?$+-,]+"));
             double score = 0;
-            score = relevanceRank(filter, titleSplit, score);
-            score = relevanceRank(filter, descriptionSplit, score);
+            score += relevanceRank(filter, titleSplit, score);
+            score += relevanceRank(filter, descriptionSplit, score);
             hashmap.put(e,score);
         }
         return sortByRelevanceRank(hashmap);
     }
 
-    private static double relevanceRank(UserPostFilter filter, String[] splitStr, double score) {
+    //this calculates the term frequency scores
+    private static double relevanceRank(UserPostFilter filter, List<String> splitStr, double score) {
         for(int i=0; i<filter.getKeywords().length; i++) {
             double count = 0;
             for(String s : splitStr) {
@@ -37,8 +39,26 @@ public class RelevanceEngine {
         return score;
     }
 
-    private static Map<String, Double> calculateIDFScore(String[] keywords) {
-        return null;
+    //calculates idf scores for all the terms in the query
+    private static Map<String,Double> calculateIDFScore(String[] keywords, List<UserPost> postList) {
+        Map<String,Double> idfScoreMap = new HashMap<>();
+        for(String s : keywords) {
+            double docCount = 0;
+            for(UserPost e : postList) {
+                List<String> titleSplit = Arrays.asList(e.getTitle().toLowerCase().split("[\\s@&.?$+-,]+"));
+                List<String> descriptionSplit = Arrays.asList(e.getDescription().toLowerCase().split("[\\s@&.?$+-,]+"));
+                if(titleSplit.contains(s.toLowerCase()) || descriptionSplit.contains(s.toLowerCase())) {
+                    docCount++;
+                }
+            }
+            double score = 1 + Math.log(postList.size()/(docCount+1));
+            idfScoreMap.put(s,score);
+        }
+        return idfScoreMap;
+    }
+
+    private static double numTermsInString(UserPost userPost, String[] keywords) {
+        return 0;
     }
 
     private static List<UserPost> sortByRelevanceRank(Map<UserPost,Double> hashmap) {
