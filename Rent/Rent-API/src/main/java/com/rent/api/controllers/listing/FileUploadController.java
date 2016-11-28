f53faa43146da3032c63acf3831bf8065219fe9b
Asad <aabdullah7757@gmail.com>
2016-11-28 09:04:21 -0800

    started working fileUploadController

--- Rent/Rent-API/src/main/java/com/rent/api/controllers/listing/FileUploadController.java
+++ Rent/Rent-API/src/main/java/com/rent/api/controllers/listing/FileUploadController.java
@@ -1,7 +1,20 @@
 package com.rent.api.controllers.listing;
 
+import com.rent.api.services.listing.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Asad on 11/27/2016.
  */
+
+@RestController
+@RequestMapping("/fileUpload")
 public class FileUploadController {
+
+    @Autowired
+    FileStorageService fileStorageService;
+
+
 }
