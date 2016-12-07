0dc7f078704be216fa3cc95dde377b9e21427d99
HertelDavid <herteldavid1@gmail.com>
2016-12-06 21:17:00 -0800

    User can now create posts from the application.

--- Rent-Android/app/src/main/java/com/rentables/testcenter/CreatePostFragment.java
+++ Rent-Android/app/src/main/java/com/rentables/testcenter/CreatePostFragment.java
@@ -1,7 +1,7 @@
 package com.rentables.testcenter;
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class CreatePostFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
--- Rent-Android/app/src/main/java/com/rentables/testcenter/HomeActivity.java
+++ Rent-Android/app/src/main/java/com/rentables/testcenter/HomeActivity.java
@@ -1,17 +1,29 @@
 package com.rentables.testcenter;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
+import android.support.v4.app.FragmentManager;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-public class HomeActivity extends AppCompatActivity {
+import java.util.concurrent.locks.ReentrantLock;
+
+import dataobject.CreateListing;
+import server.NotifyingThread;
+import server.ServerConnection;
+import server.ThreadListener;
+
+public class HomeActivity extends AppCompatActivity implements ThreadListener {
+
+    //Only want to be able to run one thread at a time for creating posts.
+    private Thread runningThread = null;
 
     @Override
     protected void onCreate(Bundle savedInstance){
@@ -50,25 +62,37 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void notifyOfThreadCompletion(NotifyingThread notifyThread) {
+
+        runOnUiThread(new Runnable(){
+            @Override
+            public void run(){
+                showPostCreatedDialog();
+            }
+        });
+
+        System.out.println("Should only reach this once");
+    }
 
     public void selectFrag(View view) {
         Fragment fr;
 
         if (view == findViewById(R.id.button1)) {
             fr = new HomeFragment();
-        } else if (view == findViewById(R.id.button2)) {
+        } /*else if (view == findViewById(R.id.button2)) {
             fr = new ButtonTwoFragment();
-        } else if (view == findViewById(R.id.button3)) {
+        }*/ else if (view == findViewById(R.id.button3)) {
             fr = new BrowseFragment();
         } else if (view == findViewById(R.id.button4)) {
             fr = new MyPostsFragment();
-        } else if (view == findViewById(R.id.button5)) {
+        }/* else if (view == findViewById(R.id.button5)) {
             fr = new ButtonFiveFragment();
-        } else if (view == findViewById(R.id.create_post_button)) {
+        }*/ else if (view == findViewById(R.id.create_post_button)) {
             fr = new CreatePostFragment();
         } else
             fr = new HomeFragment();
-        FragmentManager fm = getFragmentManager();
+        FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
         fragmentTransaction.replace(R.id.fragment_place, fr);
         fragmentTransaction.commit();
@@ -82,4 +106,84 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void createListing(View view){
+
+        //Method for creating a post.
+        //TODO UserId needs to be fixed.
+
+        System.out.println("Reached");
+
+        if(runningThread == null) {
+
+            CreateListing theListing = new CreateListing();
+
+            EditText titleText = (EditText) findViewById(R.id.title_text);
+            EditText descriptionText = (EditText) findViewById(R.id.description_text);
+            EditText priceText = (EditText) findViewById(R.id.create_post_price);
+            Spinner priceCategoryIdText = (Spinner) findViewById(R.id.rental_time_spinner);
+
+            String title = titleText.getText().toString().trim();
+            String description = descriptionText.getText().toString().trim();
+            String price = priceText.getText().toString().trim();
+            String priceCategoryId = priceCategoryIdText.getSelectedItem().toString();
+            int userId = 3; //A placeholder for now.
+
+            theListing.setTitle(title);
+            theListing.setDescription(description);
+            theListing.setPrice(Double.parseDouble(price));
+            theListing.setPriceCategoryId(getCorrectPriceCategoryId(priceCategoryId));
+            theListing.setUserId(userId);
+
+            ServerConnection<CreateListing> connection = new ServerConnection<>(theListing);
+            connection.addListener(this);
+
+            runningThread = new Thread(connection);
+            runningThread.start();
+            System.out.println("Reached Behind the wall!");
+        }
+    }
+
+    public int getCorrectPriceCategoryId(String priceCategoryId){
+
+        switch (priceCategoryId.toLowerCase()){
+
+            case "hours":
+                return 1;
+            case "days":
+                return 2;
+            case "weeks":
+                return 3;
+            case "months":
+                return 4;
+            case "semester":
+                return 5;
+        }
+
+        return 1;
+    }
+
+    private void showPostCreatedDialog(){
+
+        FragmentManager manager = getSupportFragmentManager();
+        CreatePostDialog successDialog = new CreatePostDialog();
+        successDialog.show(manager, "dialog_create_post_successful");
+    }
+
+    public void onPostCreation(View view){
+
+        runningThread = null;
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fr = new HomeFragment();
+        DialogFragment up = (DialogFragment) fm.findFragmentByTag("dialog_create_post_successful");
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+
+        if(up != null){
+
+            fragmentTransaction.remove(up);
+        }
+
+        fragmentTransaction.replace(R.id.fragment_place, fr);
+        fragmentTransaction.commit();
+
+    }
 }
